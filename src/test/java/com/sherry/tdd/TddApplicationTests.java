36cbe1e15a12b78d1a2a57bc4c45e70f510cb240
Shehroaz Aslam <shehroaz.aslam@allente.tv>
2022-09-05 22:57:33 +0500

    Initial Commit

---
+++ src/test/java/com/sherry/tdd/TddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sherry.tdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TddApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
